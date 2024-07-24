742e85cad31b3f6a1829f9a95602cec958b93f9a
fatihGunal <fatihgunal2018@gmail.com>
2024-07-24 21:05:05 +0200

    Reset

--- src/main/java/com/tree/crust/CrustApplication.java
+++ src/main/java/com/tree/crust/CrustApplication.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.HashSet;
 import com.tree.crust.fw.ClassLocator;
 import com.tree.crust.fw.LeafFactory;
+import com.tree.crust.fw.WebComponent;
 
 /**
  * Hello world!
@@ -22,9 +23,11 @@ public class CrustApplication {
             LeafFactory leafFactory = new LeafFactory();
             leafFactory.createContainer(clazzNames);
             leafFactory.printObjects();
-//            leafFactory.manualTestOne();
-//            leafFactory.manualTestTwo();
+            // leafFactory.manualTestOne();
+            // leafFactory.manualTestTwo();
 
+            WebComponent wComponent = new WebComponent();
+            wComponent.handleSocket();
         } catch (IOException | IllegalAccessException ex) {
             System.out.println(ex);
         }
--- src/main/java/com/tree/crust/fw/WebComponent.java
+++ src/main/java/com/tree/crust/fw/WebComponent.java
@@ -1,5 +1,12 @@
 package com.tree.crust.fw;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * WebComponent
  * Here, we implement @RestController annotation
@@ -8,7 +15,16 @@ package com.tree.crust.fw;
  */
 public class WebComponent {
 
-    public void handleSocket() {
+    public void handleSocket() throws IOException {
+        ServerSocket serverSocket = new ServerSocket(8080);
+        Socket clientSocket = serverSocket.accept();
+        BufferedReader bReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        PrintWriter pWriter = new PrintWriter(clientSocket.getOutputStream(), true);
 
+        String inputLine;
+        while ((inputLine = bReader.readLine()) != null) {
+            pWriter.println("Connection established!");
+            System.out.println("Connection established! :)");
+        }
     }
 }
